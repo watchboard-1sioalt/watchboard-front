240abe1a6a5cfc75538d4fdd7c7181c23caebdb9
Twixty <twixty.contact@gmail.com>
2026-06-12 16:37:46 +0200

    ➕Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Navbar from './components/Navbar/Navbar'
 
 // Router (permet de gérer les différentes pages)
 function App() {
   return (
-    <>
+    <main className="flex flex-col">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
-    </>
+    </main>
   )
 }
 
